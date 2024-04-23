eb0fc6996e62f3480ee527edefc68e781077eddd
Tanagends <tanagends@gmail.com>
2024-04-23 23:44:26 +0200

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,7 +1,7 @@
  
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
     let task = true;
