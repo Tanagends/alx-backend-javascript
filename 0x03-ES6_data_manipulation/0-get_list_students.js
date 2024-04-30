c70509591cf452d64a6688faefd3377f9a1759e8
Tanagends <tanagends@gmail.com>
2024-05-01 00:03:44 +0200

    data manipulation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,7 @@
-
 export default function getListStudents() {
-  let first = {id: 1, firstName: 'Guillaume', location: 'San Francisco',};
-  let second = {id: 2, firstName: 'James', location: 'Columbia',};
-  let third = {id: 5, firstName: 'Serena', location: 'San Francisco',};
+  const first = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const second = { id: 2, firstName: 'James', location: 'Columbia' };
+  const third = { id: 5, firstName: 'Serena', location: 'San Francisco' };
 
   return [first, second, third];
 }
--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -10,3 +10,5 @@ export default function createInt8TypedArray(length, position, value) {
   view[position] = value;
   return buffer;
 }
+
+console.log(createInt8TypedArray(10, 2, 89));
