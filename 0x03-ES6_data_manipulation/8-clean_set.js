f884e70e8df588d86b358f7bbed08559838391cd
Tanagends <tanagends@gmail.com>
2024-05-01 01:02:41 +0200

    data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
-
-
 export default function cleanSet(set, startString) {
   if (startString === '') {
     return '';
   }
-  return (([...set].filter(el => el.startsWith(startString))).map(ele => ele.slice(startString.length))).join('-');
+  return (([...set]
+    .filter((el) => el.startsWith(startString)))
+    .map((ele) => ele.slice(startString.length))).join('-');
 }
 
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
